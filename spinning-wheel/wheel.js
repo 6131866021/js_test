0a83d92a3d857702643cbc01535db098f13b5f7f
6131866021 <6131866021@student.chula.ac.th>
2021-02-04 23:48:06 +0700

    init

--- spinning-wheel/wheel.js
+++ spinning-wheel/wheel.js
@@ -1,14 +1,26 @@
 function myfunction() {
-    var x = 1024;   // min value
-    var y = 9999;   // max value
+    var x = 1000;   // min value
+    var y = 1689;   // max value
     
     var deg = Math.floor(Math.random() * (x-y)) + y;
-    console.log(deg);
+    var color, range;
+    if(deg < 1690 && deg > 1331) range = deg - 360;
+    else range = deg;
+
+    if(range < 1016) color = 'red';
+    else if((range > 1105 && range < 1151) || (range > 1195 && range < 1241)) color = 'green'
+    else color = 'blue'
+
+    console.log(deg, color);
     document.getElementById("box").style.transform = "rotate("+deg+"deg)";
 
-    var element = documet.getElementById('mainbox');
-    element.classList.remove('animate');
+    /* var element = documet.getElementById('mainbox');
+    // element.classList.remove('animate');
     setTimeout(function() {
         element.classList.add('animate');
     }, 5000);
+    */
+
+    document.getElementById('result').innerHTML = color;
+    
 }
