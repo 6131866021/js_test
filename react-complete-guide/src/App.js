aaf23c0ff64beb9f3ce89deb12b275ce85fc7294
6131866021 <6131866021@student.chula.ac.th>
2021-02-10 02:30:26 +0700

    init

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,23 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends Component {
+  render() {
+    return (                                      // JSX not html 
+      <div className="App">
+        <h1>Hi, I'm a React app</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+  }
+}
+
+export default App;                                 // ES6 JS
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +38,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+*/
