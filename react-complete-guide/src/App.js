835e530600d3d467f686654a6a0f7846da0c7fa5
6131866021 <6131866021@student.chula.ac.th>
2021-02-12 00:53:37 +0700

    init

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,24 +12,25 @@ const App = props => {
   });
 
   const [otherState, setOtherState] = useState('some other value');
+  const [showPersons, setShowPersons] = useState(false);
 
-  console.log(personsState, otherState);
+  console.log(personsState, otherState, showPersons);
 
-  const switchNameHandler = (newName) => {
+  const switchNameHandler = ( newName ) => {
     console.log('Was clicked!');
     setPersonsState({
       persons: [
         { name: newName, age: 90 },
-        { name: 'Nat', age: 25 },
-        { name: 'Nani', age: 39 }
+        { name: 'Nat', age: 25 }
       ],
-      otherState: personsState.setotherState
+      otherState: personsState.setotherState,
+      showPersons: personsState.setShowPersons
     });
 
     // DON'T DO: this.state.personsp[0].name = 'Maximum';
   };
 
-  const nameChangedHandler = (event) => {
+  const nameChangedHandler = ( event ) => {
     console.log('Name Changed');
     setPersonsState({
       persons: [
@@ -39,28 +40,106 @@ const App = props => {
     });
   }
 
-  return (                                      // JSX not html 
+  const togglePersonsHandler = () => {
+    console.log('toggle');
+    const doesShow = showPersons;
+    setPersonsState({
+      showPersons: !doesShow
+    });
+    let persons = null;
+    if( showPersons ) {
+      console.log("showPersons");
+      {personsState.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.age} />
+      })}
+    }
+  }
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
+  /* 
+  let persons = null;
+    if( showPersons ) {
+      persons = (
+        <div>
+        <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}
+          click={switchNameHandler}
+          changed={nameChangedHandler}>
+            My Hobbies: Racing
+        </Person>
+        <Person
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age}>
+        </Person>
+      </div>
+      )
+    }
+    */
+
+  return (                                      // JSX not html
+
     <div className="app">
       <h1>Hi, I'm a React app</h1>
-      <button onClick={() => switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
-      <Person 
-        name={personsState.persons[0].name} 
-        age={personsState.persons[0].age}
-        click={switchNameHandler}
-        changed={nameChangedHandler}>
-          My Hobbies: Racing
-      </Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
+      <button
+        style={style}
+        onClick={() => switchNameHandler.bind(this, 'Maximilian')}>Switch Name
+      </button>
+      <button 
+        style={style} 
+        onClick={togglePersonsHandler}>Toggle
+      </button>
+      {personsState.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.age}
+              click={switchNameHandler}
+              changed={nameChangedHandler} />
+      })}
     </div>
   )
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
 }
 
 export default App;                                 // ES6 JS
 
+  /* 
+
+     <div>
+          {personsState.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.age} />
+          })}
+        </div>
 
 
-/*
+  <div>
+    <Person 
+    name={personsState.persons[0].name} 
+    age={personsState.persons[0].age}
+    click={switchNameHandler}
+    changed={nameChangedHandler}>
+      My Hobbies: Racing
+    </Person>
+    <Person
+      name={personsState.persons[1].name} 
+      age={personsState.persons[1].age}>
+    </Person>
+  </div>
+  
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+
+
+
 function App() {
   return (
     <div className="App">
